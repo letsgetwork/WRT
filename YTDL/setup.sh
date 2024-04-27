58c906ea6bbdf8f4797f371633c21fb3c3069124
letsgetwork <asharirhmn@gmail.com>
2024-04-27 20:55:28 +0800

    Update setup.sh

--- YTDL/setup.sh
+++ YTDL/setup.sh
@@ -1,1 +1,24 @@
+#!/bin/ash
+# Installation script by @ASHARIRHMN.
 
+DIR=/usr/bin
+
+finish(){
+    clear
+    echo ""
+    echo "UNDUH YTDL SELESAI ❤️"
+    echo ""
+    echo "Ketik ytdl untuk mulai aplikasi"
+    echo ""
+    echo ""
+}
+
+download_files()
+{
+    clear
+    echo "Mengunduh YTDL... mohon tunggu"
+    wget -O $DIR/ytdl https://github.com/letsgetwork/WRT/raw/main/YTDL/ytdl && chmod +x $DIR/ytdl
+    finish
+}
+
+download_files
