d9370d9552f9d65076305ca4c26a35f770221692
letsgetwork <asharirhmn@gmail.com>
2024-04-27 14:44:04 +0800

    Update setup.sh

--- Freeless/setup.sh
+++ Freeless/setup.sh
@@ -1,1 +1,38 @@
+#!/bin/ash
+# Installation script by @ASHARIRHMN.
 
+DIR=/openclash/config/
+
+
+finish(){
+clear
+	echo ""
+    echo "UNDUH CONFIG SELESAI ❤️"
+    echo ""
+    echo "Silahkan cek pada OpenClash & pilih config tersebut untuk menggunakan Freeless config"
+    sleep 3
+    echo ""
+    echo "HAPPY INTERNET BROU!!!!"
+    echo ""
+    echo ""
+}
+
+download_files()
+{
+    	clear
+  	echo "Mengunduh config... mohon tunggu"
+   	wget -O $DIR/Freeless.yaml https://raw.githubusercontent.com/letsgetwork/WRT/main/Freeless/FreelessB.yaml && chmod +x $DIR/Freeless.yaml
+    	finish
+}
+
+echo ""
+echo "Freeless by @asharirhmn // Install Config dari Github ARI @letsgetwork"
+
+while true; do
+    read -p "Dokumen akan disimpan pada folder $DIR. Tetap lanjutkan? (y/n)? " yn
+    case $yn in
+        [Yy]* ) download_files; break;;
+        [Nn]* ) exit;;
+        * ) echo "Jawab dengan ketik 'y' or 'n'.";;
+    esac
+done
