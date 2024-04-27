5b749d0147d3cf3643288ba5341cd9c8b94e3fa5
letsgetwork <asharirhmn@gmail.com>
2024-04-27 11:46:45 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# INSTALL VIA TERMINAL
+Copy & paste command berikut
+
+### `bash -c "$(wget -qO - 'https://asharirhmn.com/bughunter')"`
+
+
+setelah berhasil install, silahkan ketik **bug** untuk memulai. enjoyy
+
+
+#### Catatan
+1. Pastikan kamu memiliki kureg pada simcard mu
+2. Jika menggunakan inject 0kuota, pastikan pada aplikasi tunneling mu (OPENCLASH) pada mode redir-host. Jika menggunakan fake-ip, aplikasi ini tidak bisa berjalan dengan baik.
+3. Ada pertanyaan, kontak via telegram **https://t.me/asharirhmn**
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/ash
+# Installation script by @ASHARIRHMN.
+
+DIR=/usr/bin
+
+
+finish(){
+clear
+	echo ""
+    echo "INSTALL SELESAI ❤️"
+    echo ""
+    echo "Mulai jalankan dengan ketik bug dan enter pada terminal"
+    sleep 3
+    echo ""
+    echo "HAPPY HUNTING BROU!!!!"
+    echo ""
+    echo ""
+}
+
+download_files()
+{
+    	clear
+  	echo "Mengunduh dokumen... mohon tunggu"
+   	wget -O $DIR/bug https://raw.githubusercontent.com/letsgetwork/WRT/main/bug && chmod +x $DIR/bug
+    	finish
+}
+
+echo ""
+echo "BUG Hunter by @asharirhmn // Install Script dari Github ARI @letsgetwork"
+
+while true; do
+    read -p "Dokumen akan disimpan pada folder $DIR. Tetap lanjutkan? (y/n)? " yn
+    case $yn in
+        [Yy]* ) download_files; break;;
+        [Nn]* ) exit;;
+        * ) echo "Jawab dengan ketik 'y' or 'n'.";;
+    esac
+done
