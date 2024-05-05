c170f1c58dcf4a691ce3c1883736fea594a29d4e
letsgetwork <asharirhmn@gmail.com>
2024-05-05 14:16:00 +0800

    package wajib ari punya

---
+++ Others/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/ash
+# Installation script by @ASHARIRHMN.
+
+DIR=/usr/bin
+
+finish(){
+    clear
+    echo ""
+    echo "UNDUH OTHER TOOLS LAIN SELESAI ❤️"
+    echo ""
+    echo "Silahkan cek pada /usr/bin/""
+    echo ""
+    echo ""
+}
+
+download_files()
+{
+    clear
+    echo "Tunggu sebentar"
+    sleep 5
+    echo "Pastikan koneksi baik"
+    sleep 10
+    echo "Mengunduh OTHER PACKAGE... mohon tunggu"
+    wget -O $DIR/jam https://raw.githubusercontent.com/letsgetwork/WRT/main/Others/jam && chmod +x $DIR/jam
+    sleep 5
+    wget -O $DIR/hilink https://raw.githubusercontent.com/letsgetwork/WRT/main/Others/hilink && chmod +x $DIR/hilink
+    sleep 5
+    wget -O $DIR/lite_watchdog.sh https://raw.githubusercontent.com/letsgetwork/WRT/main/Others/lite_watchdog.sh && chmod +x $DIR/lite_watchdog.sh
+    sleep 5
+    finish
+}
+
+download_files
