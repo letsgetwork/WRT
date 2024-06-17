be30e467d574bf1328ca1f14b0e2a954790daffd
letsgetwork <asharirhmn@gmail.com>
2024-06-17 21:01:43 +0800

    Update setup.sh

--- AON/setup.sh
+++ AON/setup.sh
@@ -17,8 +17,11 @@ finish(){
 download_files()
 {
     clear
-    echo "Mengunduh AON... mohon tunggu"
+    echo "Mengunduh AON & dependency hilink... mohon tunggu"
     wget -O $DIR/aon https://raw.githubusercontent.com/letsgetwork/WRT/main/AON/aon && chmod 0755 $DIR/aon
+    sleep 5
+    wget -O $DIR/hilink https://raw.githubusercontent.com/letsgetwork/WRT/main/Others/hilink && chmod 0755 $DIR/hilink
+    sleep 5
     finish
 }
 
